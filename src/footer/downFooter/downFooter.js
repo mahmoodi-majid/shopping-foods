26e9faf06a737005af03c416ee38aff93b41515c
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-04-18 17:43:07 +0430

    finished design

--- src/footer/downFooter/downFooter.js
+++ src/footer/downFooter/downFooter.js
@@ -3,7 +3,11 @@ import './downFooter.css'
 export default function DownFooter() {
   return (
     <div className='downFooter'>
-           
+           <ul>
+            <li className='downFooterItem'>© Untitled. All : rights reserved</li>
+            <li className='downFooterItem'>Design:TEMPLATED</li>
+            <li className='downFooterItem'>ImagesUnsplash</li>
+           </ul>
     </div>
   )
 }
